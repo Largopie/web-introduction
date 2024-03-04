e1f1031a370a957e8af2bb41f2a9db483d98f43a
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-04 22:26:41 +0900

    feat: 방명록에 등록한 사람의 이름, 체크한 개수 출력 로직 구현

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const registrationButton = document.querySelector('#registration');
+const bestSongsElement = document.getElementsByClassName('best-songs');
+const bestSongs = [];
+
+for (let i = 0; i < bestSongsElement.length; i++) {
+  bestSongs.push(bestSongsElement[i].innerText);
+}
+
+registrationButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name').value;
+  const checkboxs = document.querySelectorAll('input[type="checkbox"]');
+  const checked = [];
+
+  checkboxs.forEach((checkbox) => {
+    if (checkbox.checked) {
+      const title = document.querySelector(`label[for="${checkbox.id}"]`).innerText;
+      checked.push(title);
+    }
+  });
+
+  alert(`${name} 님, 저와 ${checked.length}개의 취향이 같으시네요!\n ${checked.map((title, idx) => `${idx + 1}. ${title}`).join('\n')}`);
+});
